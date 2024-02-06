4102b55e8d986625a98231ed72c542c120c9efc6
a7medbehiry <a7medbehiry@gmail.com>
2024-02-06 18:48:34 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SafeReturnApp());
+}
+
+class SafeReturnApp extends StatelessWidget {
+  const SafeReturnApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
