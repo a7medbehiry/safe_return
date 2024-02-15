414e04e68335f512ff652af74c5b842dcf38163b
a7medbehiry <a7medbehiry@gmail.com>
2024-02-15 14:34:37 +0200

    refactor code

--- lib/core/utils/widgets/custom_text_field.dart
+++ lib/core/utils/widgets/custom_text_field.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:safe_return/constants.dart';
 import 'package:safe_return/core/utils/styles.dart';
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   const CustomTextField({
     super.key,
     this.hintText,
@@ -32,27 +32,57 @@ class CustomTextField extends StatelessWidget {
 
   final int? maxLines;
   final Widget? label;
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  late FocusNode _focusNode;
+  bool _isPrefixIconVisible = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = FocusNode();
+    _focusNode.addListener(_handleFocusChange);
+  }
+
+  @override
+  void dispose() {
+    _focusNode.removeListener(_handleFocusChange);
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  void _handleFocusChange() {
+    setState(() {
+      _isPrefixIconVisible = !_focusNode.hasFocus;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: width,
-      height: height,
+      width: widget.width,
+      height: widget.height,
       child: TextField(
+        focusNode: _focusNode,
         cursorColor: kPrimaryColor,
-        maxLines: maxLines,
-        onTap: onTap,
-        controller: controller,
-        readOnly: readOnly,
-        keyboardType: keyboardType,
-        onChanged: onChanged,
+        maxLines: widget.maxLines,
+        onTap: widget.onTap,
+        controller: widget.controller,
+        readOnly: widget.readOnly,
+        keyboardType: widget.keyboardType,
+        onChanged: widget.onChanged,
         style: Styles.textStyleReg16.copyWith(
           color: Colors.black,
         ),
-        obscureText: obscureText,
+        obscureText: widget.obscureText,
         decoration: InputDecoration(
           contentPadding:
               const EdgeInsets.symmetric(vertical: 5, horizontal: 14),
-          fillColor: fillColor,
+          fillColor: widget.fillColor,
           filled: true,
           border: const OutlineInputBorder(
             borderRadius: BorderRadius.all(
@@ -61,13 +91,13 @@ class CustomTextField extends StatelessWidget {
               ),
             ),
           ),
-          prefixIcon: prefixIcon,
+          prefixIcon: _isPrefixIconVisible ? widget.prefixIcon : null,
           prefixIconColor: kPrimaryColor,
-          label: label,
+          label: widget.label,
           labelStyle: Styles.textStyleReg16.copyWith(
             color: Colors.black,
           ),
-          hintText: hintText,
+          hintText: widget.hintText,
           hintStyle: Styles.textStyleReg16.copyWith(
             color: Colors.black,
           ),
