e93816a4d093121648cdb5840db7a84957d3b880
a7medbehiry <a7medbehiry@gmail.com>
2024-06-29 23:55:44 +0300

    refactor code

--- lib/Features/notificationView/presentation/manager/firebase_notifications.dart
+++ lib/Features/notificationView/presentation/manager/firebase_notifications.dart
@@ -1,5 +1,4 @@
 import 'dart:developer';
-
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:go_router/go_router.dart';
 import 'package:safe_return/core/utils/app_router.dart';
@@ -7,37 +6,49 @@ import 'package:safe_return/core/utils/functions/custom_snack_bar.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class FirebaseNotifications {
-  // Create instance of FBM
-  final _firebaseMessaging = FirebaseMessaging.instance;
+  // Create an instance of FirebaseMessaging
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
 
-  // Initialize notification for this app or device
+  // Initialize notifications for this app or device
   Future<void> initNotifications() async {
+    // Request permission for notifications
     await _firebaseMessaging.requestPermission();
+
+    // Get the FCM token and log it
     String? fcmToken = await _firebaseMessaging.getToken();
     log("FCM Token: $fcmToken");
-    handleBackgroundNotifications();
 
-
+    // Save the FCM token to shared preferences
     final SharedPreferences preferences = await SharedPreferences.getInstance();
-    preferences.setString('fcmToken', "$fcmToken");
+    await preferences.setString('fcmToken', fcmToken ?? '');
 
+    // Handle notifications
+    handleBackgroundNotifications();
   }
 
-  // handle notifications when received
+  // Handle notifications when received
   void handleMessage(RemoteMessage? message) {
     if (message == null) return;
 
-    AppRouter.navigatorKey.currentState!.context.goNamed('notificationView');
+    // Navigate to the notification view
+    AppRouter.navigatorKey.currentState?.context.goNamed('notificationView');
   }
 
-  //handle notifications in case of app is terminated
-  Future handleBackgroundNotifications() async {
+  // Handle notifications in case the app is terminated
+  Future<void> handleBackgroundNotifications() async {
+    // Handle the message if the app was opened from a terminated state
     FirebaseMessaging.instance.getInitialMessage().then(handleMessage);
+
+    // Handle the message if the app was brought to the foreground
     FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
+
+    // Handle the message when the app is in the foreground
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      log("Title: ${message.notification!.title}");
-      log("Body: ${message.notification!.body}");
-      SnackBarManager.showSnackBarNotification(message.notification?.body);
+      if (message.notification != null) {
+        log("Title: ${message.notification!.title}");
+        log("Body: ${message.notification!.body}");
+        SnackBarManager.showSnackBarNotification(message.notification?.body);
+      }
     });
   }
 }
