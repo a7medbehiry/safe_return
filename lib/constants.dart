c2938d5230db8d87f23d56cb2e76f3e4c1a5682e
a7medbehiry <a7medbehiry@gmail.com>
2024-07-02 03:56:43 +0300

    refactor code

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,7 +5,7 @@ const kSecondColor = Color(0xffC1AF92);
 const kThirdColor = Color(0xffEEE8D8);
 
 const baseUrl = 'https://safe-return.onrender.com/api/v1/';
-// const baseUrl = 'http://localhost:3000/api/v1/';
+// const baseUrl = 'http://10.0.2.2:3000/api/v1/';
 
 enum Gender { female, male }
 
